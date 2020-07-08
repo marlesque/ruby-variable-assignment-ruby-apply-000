ce743107b99d99fc101b6777bcd1dcebda26a055
candescent-method-1839 <mqu000@citymail.cuny.edu>
2020-07-08 00:12:08 +0000

    Automatically backed up by Learn

--- spec/variable_spec.rb
+++ spec/variable_spec.rb
@@ -2,7 +2,7 @@
 describe "./variable.rb" do
 
   it "defined a local variable called greeting and set it equal to 'Hello World'" do
-    greeting = "Hello World!"
+    greeting = "Hello World"
 
     expect(greeting).to eq("Hello World")
   end
